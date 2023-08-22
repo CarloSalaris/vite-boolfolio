6dbe94d34d4680f55cb179f414d4cdf9ddf66526
CarloSalaris <carloa.salaris@gmail.com>
2023-08-22 18:32:02 +0200

    Header common to all pages

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,19 @@
 <script>
-import axios from 'axios';
+import AppHeader from './components/AppHeader.vue';
 
 const API = 'http://localhost:8000/api/v1';
 
 export default {
   name: 'App',
+  components: {
+    AppHeader
+  }
 }
 
 </script>
 
 <template>
+  <AppHeader/>
   <router-view></router-view>
 </template>
 
---
+++ src/components/AppHeader.vue
@@ -0,0 +1,11 @@
+<script>
+export default {
+    name:'AppHeader'
+}
+</script>
+
+<template>
+    <div class="p-3 text-primary-emphasis bg-primary-subtle border border-primary-subtle mb-4">
+        <h1>Hello! I'm the Header...</h1>
+    </div>
+</template>
