5c429dcc8511e04e8e04ab68deefc38338a35fa2
CarloSalaris <carloa.salaris@gmail.com>
2023-08-22 16:37:25 +0200

    Images + description in projects

--- src/components/ProjectCard.vue
+++ src/components/ProjectCard.vue
@@ -11,7 +11,10 @@ export default {
 </script>
 
 <template>
-    <div class="card col-3 m-3">
-        <h1>{{ project.title }}</h1>
+    <div class="card col-sm-12 col-md-5 col-xl-3 m-3">
+        <h3>{{ project.title }}</h3>
+        <img :src="(project.main_picture && project.main_picture != 0) ? 'http://localhost:8000/storage/' + project.main_picture : 'http://localhost:8000/storage/img/default.jpg'" alt="">
+        <p>{{ project.description }}</p>
+        
     </div>
 </template>
