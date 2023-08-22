f272f25d74493f54bd1209ebd4da3b8fbec5eda0
CarloSalaris <carloa.salaris@gmail.com>
2023-08-22 18:24:56 +0200

    ProjectCard router-link to ProjectDetails

--- src/components/ProjectCard.vue
+++ src/components/ProjectCard.vue
@@ -11,10 +11,30 @@ export default {
 </script>
 
 <template>
-    <div class="card col-sm-12 col-md-5 col-xl-3 m-3">
-        <h3>{{ project.title }}</h3>
-        <img :src="(project.main_picture && project.main_picture != 0) ? 'http://localhost:8000/storage/' + project.main_picture : 'http://localhost:8000/storage/img/default.jpg'" alt="">
-        <p>{{ project.description }}</p>
+    <div class="col col-sm-12 col-md-5 col-xl-3">
+        <div class="card">
+            <router-link
+                :to="{
+                    name: 'project',
+                    params: {id: project.id}
+                }">
+                <div class="card-header">
+                    <h3>{{ project.title }}</h3>
+                </div>
+                <img :src="(project.main_picture && project.main_picture != 0) ? 'http://localhost:8000/storage/' + project.main_picture : 'http://localhost:8000/storage/img/default.jpg'" style="
+                                width: 100%;
+                                object-fit: cover;
+                                aspect-ratio: 3/2;" alt="">
+                <p>{{ project.description }}</p>
+            </router-link>
         
+        </div>
     </div>
 </template>
+
+<style scoped> 
+    a {
+        text-decoration: none;
+        color: inherit;
+    }
+</style>
