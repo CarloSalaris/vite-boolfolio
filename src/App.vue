3b67d7d819fe931320254f0b766fe9be88f897fb
CarloSalaris <carloa.salaris@gmail.com>
2023-08-22 18:21:38 +0200

    router-vew in App.vue

--- src/App.vue
+++ src/App.vue
@@ -1,43 +1,15 @@
 <script>
-
-import ProjectCard from './components/ProjectCard.vue'
 import axios from 'axios';
 
 const API = 'http://localhost:8000/api/v1';
 
 export default {
   name: 'App',
-  components: {
-    ProjectCard
-  },
-  data() {
-    return {
-      projects: []
-    }
-  },
-
-  mounted() {
-    axios.get(API + '/projects-index')
-      .then(res => {
-
-        /*  const data = res.data.projects;
- 
-         this.projects = data.data; */
-
-        this.projects = res.data.projects;
-      })
-      .catch(err => console.error(err));
-  }
 }
 
 </script>
 
 <template>
-  <div class="container-fluid text-center">
-    <h1>Projects</h1>
-    <div class="row justify-content-center">
-      <ProjectCard v-for="project in projects" :key="project.id" :project="project" />
-    </div>
-  </div>
+  <router-view></router-view>
 </template>
 
